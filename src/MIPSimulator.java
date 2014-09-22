d614233233602cf80286e4ba98981303706bf6a8
José Pablo Flores G <josepablofg@gmail.com>
2014-09-22 15:45:46 -0600

    Updated .gitignore

---
+++ src/MIPSimulator.java
@@ -0,0 +1,159 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Clase que simula un procesador de 1 núcleo MIPS
+ *
+ */
+public class MIPSimulator {
+	private int[] instructionMem; // Memoria de instrucciones
+	private int[] dataMem;        // Memoria de datos
+	private int[] R;              // Registros del procesador
+	private CyclicBarrier clock;  // Reloj del sistema
+	private int clockCycle;       // Ciclo actual de reloj
+	private int PC;               // Contador del programa / Puntero de instrucciones
+	
+	/**
+	 * Construye una instancia de MIPSimulator e inicializa sus atributos.
+	 */
+	public MIPSimulator(){
+		this.R = new int[32];
+		this.PC = 0;
+		this.clock = new CyclicBarrier(4); // El 4 no sé...
+		this.dataMem = new int[200];
+		this.clockCycle = 0;
+		this.instructionMem = new int[400];
+		for(int i = 0; i < instructionMem.length; i++){
+			instructionMem[i] = -1;
+			dataMem[i%200] = -1;
+			R[i%32] = -1;
+		}
+	}
+	
+	/**
+	 * Ejecuta el programa especificado.
+	 * @param program - Archivo .txt con lenguaje máquina (para MIPS) en decimal. 
+	 */
+	public void run(File program){
+		try{
+			// Guarda las instrucciones del archivo en la memoria
+			Scanner scanner = new Scanner(program);
+			for(int i = 0; i < instructionMem.length; i++){
+				if(scanner.hasNext()){
+					instructionMem[i] = scanner.nextInt();
+				}else{
+					instructionMem[i] = -1;
+				}
+			}
+			scanner.close();
+		}catch(FileNotFoundException e){
+			System.err.println("Error abriendo el archivo del programa.");
+		}
+		// Imprime el programa que se va a ejecutar
+		printProgram();
+		
+		//El programa se ejecuta hasta toparse con una instruccion "FIN"
+		while(clockCycle == 0){//instructionMem[PC] != 63){
+			System.out.println("Ciclo: " + clockCycle);
+			PC = executeInstruction(instructionMem[PC], instructionMem[PC+1], instructionMem[PC+2], instructionMem[PC+3]);
+			clockCycle++;
+		}
+		printState();
+	}
+	
+	public int executeInstruction(int instruction, int X, int Y, int Z){
+		int nextPC = 0;
+		switch(instruction){
+			case 0:
+				// LL RX, n(RY)
+			break;
+			case 1:
+				// SC RX, n(RY)
+			break;
+			case 2:
+				// JR RX
+			break;
+			case 3:
+				// JAL n
+			break;
+			case 4:
+				// BEQZ RX, ETIQ
+			break;
+			case 5:
+				// BNEZ RX, ETIQ
+			break;
+			case 8:
+				// DADDI RX, RY, #n
+			break;
+			case 12:
+				// DMUL RX, RY, RZ
+			break;
+			case 14:
+				// DDIV RX, RY, RZ
+			break;
+			case 32:
+				// DADD RX, RY, RZ
+			break;
+			case 34:
+				// DSUB RX, RY, RZ
+			break;
+			case 35:
+				// LW RX, n(RY)
+			break;
+			case 43:
+				// SW RX, n(RY)
+			break;
+			case 63:
+				// FIN
+			break;
+			default:
+			break;
+		}
+		
+		return nextPC;
+	}
+	
+	
+	/**
+	 * Despliega en consola el estado actual de:
+	 *   - Ciclos de reloj ejecutados.
+	 *   - Contenido en los registros.
+	 *   - Contenido en la memoria de datos.
+	 */
+	private void printState(){
+		System.out.println("Ciclos ejecutados: " + clockCycle);
+		System.out.println("\n+++++CONTENIDO EN REGISTROS+++++");
+		for(int i = 0; i < R.length; i++){
+			System.out.println("R" + i + ": " + R[i] + " ");
+		}
+		System.out.println("\n+++++CONTENIDO EN MEMORIA+++++");
+		for(int i = 0; i < 200; i+=10){
+			for(int j = 0; j < 10; j++){
+				System.out.print(dataMem[i+j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * Despliega el contenido de la memoria de instrucciones,
+	 * es decir, el programa a ejecutar.
+	 */
+	private void printProgram(){
+		boolean termino = false;
+		System.out.println("+++++PROGRAMA A EJECUTAR+++++");
+		for(int i = 0; i < 400 && termino == false; i+=4){
+			for(int j = 0; j < 4; j++){
+				if(instructionMem[i+j] == -1){
+					termino = true;
+				}
+				else{
+					System.out.print(instructionMem[i+j] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFileChooser;
+
+/**
+ * Simulador MIPS
+ * 
+ * @author Alexis Alvarado   - A90280
+ * @author Jose Pablo Flores - B02409
+ * @author Roy Ramos         - A85185
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+		MIPSimulator sim = new MIPSimulator();
+
+		JFileChooser fileChooser = new JFileChooser();
+		if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+			sim.run(fileChooser.getSelectedFile());
+		}
+	}
+}
